13a02ae7ac3d3d7ea883b7af89d6173928897072
ohin03 <nfohin@gmail.com>
2026-04-17 19:51:56 +0600

    Update for link

--- config/db.js
+++ config/db.js
@@ -1,10 +1,25 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
+ 
+    const reset = "\x1b[0m";
+    const cyan = "\x1b[36m";
+    const red = "\x1b[31m";
+    const bold = "\x1b[1m";
+
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected: ${conn.connection.host}`.bgMagenta.white);
+
+    
+        console.log(`${bold}${cyan}>> MongoDB Connected: ${conn.connection.host}${reset}`);
+        
     } catch (error) {
-        console.log(`Error in mongoDB: ${error}`.bgRed.white);
+      
+        console.error(`${bold}${red}!! MongoDB Connection Error: ${error.message}${reset}`);
+        
+     
+        process.exit(1);
     }
-}
+};
+
 export default connectDB;
